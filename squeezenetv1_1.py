20b316b7347d942e98f42dcfd505a21768e95f47
Hung Nguyen <hungnguyen94@gmail.com>
2016-12-18 18:46:21 +0100

    Add Theano compatibility

--- squeezenetv1_1.py
+++ squeezenetv1_1.py
@@ -3,15 +3,30 @@ from keras.models import Model
 from keras.layers import Input, Convolution2D, MaxPooling2D, Dropout, GlobalAveragePooling2D, merge, Activation, ZeroPadding2D
 from keras.layers import AveragePooling2D, Flatten
 from keras.layers.normalization import BatchNormalization
+from keras import backend as K
+
 
 def FireModule(s_1x1, e_1x1, e_3x3, name):
-    """
+    """FireModule
+
         Fire module for the SqueezeNet model. 
         Implements the expand layer, which has a mix of 1x1 and 3x3 filters, 
         by using two conv layers concatenated in the channel dimension. 
 
+    :param s_1x1: Number of 1x1 filters in the squeeze layer
+    :param e_1x1: Number of 1x1 filters in the expand layer
+    :param e_3x3: Number of 3x3 filters in the expand layer
+    :param name: Name of the fire module
+    :return: 
         Returns a callable function
     """
+    # Concat on the channel axis. TensorFlow uses (rows, cols, channels), while
+    # Theano uses (channels, rows, cols).
+    if K.image_dim_ordering() == 'tf': 
+        concat_axis = 3
+    else: 
+        concat_axis = 1
+
     def layer(x):
         squeeze = Convolution2D(s_1x1, 1, 1, activation='relu', init='glorot_uniform', name=name+'/squeeze1x1')(x)
         squeeze = BatchNormalization(name=name+'/squeeze1x1_bn')(squeeze)
@@ -22,15 +37,26 @@ def FireModule(s_1x1, e_1x1, e_3x3, name):
         # Pad the border with zeros. Not needed as border_mode='same' will do the same.
         # expand_3x3 = ZeroPadding2D(padding=(1, 1), name=name+'_expand_3x3_padded')(squeeze)
         expand_3x3 = Convolution2D(e_3x3, 3, 3, border_mode='same', activation='relu', init='glorot_uniform', name=name+'/expand3x3')(squeeze)
-
-        expand_merge = merge([expand_1x1, expand_3x3], mode='concat', concat_axis=3, name=name+'/concat')
+        # Concat in the channel dim
+        expand_merge = merge([expand_1x1, expand_3x3], mode='concat', concat_axis=concat_axis, name=name+'/concat')
         return expand_merge
     return layer
     
 
+def SqueezeNet(nb_classes, rows=227, cols=227, channels=3):
+    """
+        SqueezeNet v1.1 implementation
+    :param nb_classes: Number of classes
+    :param rows: Amount of rows in the input
+    :param cols: Amount of cols in the input
+    :param channels: Amount of channels in the input
+    :returns: SqueezeNet model
+    """
+    if K.image_dim_ordering() == 'tf': 
+        input_shape = (rows, cols, channels)
+    else: 
+        input_shape = (channels, rows, cols)
 
-def SqueezeNet(nb_classes, input_shape=(227, 227, 3)): 
-    # Use input shape (227, 227, 3) instead of the (224, 224, 3) shape in the paper. 
     input_image = Input(shape=input_shape)
     # conv1 output shape = (113, 113, 64)
     conv1 = Convolution2D(64, 3, 3, activation='relu', subsample=(2, 2), init='glorot_uniform', name='conv1')(input_image)
--- train_squeezenet.py
+++ train_squeezenet.py
@@ -1,79 +1,77 @@
 #!/usr/bin/env python
 
 from __future__ import division
-from squeezenet import SqueezeNet
-from keras.utils.np_utils import to_categorical
+from squeezenetv1_1 import SqueezeNet
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
-import numpy as np
 import os
-import cv2
-import random
 
-images_dir = './images'
-weights_file = './weights.h5'
+training_dir = '../images/training'
+val_dir = '../images/validation'
+weights_file = '../models/squeezenet_v1.1_weights.h5'
+
 initial_epoch = 0
-nb_epoch = 7
-batch_size = 32
-samples_per_epoch=2300
-nb_val_samples=32
+nb_epoch = 1
+batch_size = 64
+samples_per_epoch = 2300
+nb_val_samples = 300
 
-class_mapping = {
-    'coca_cola_bottles': 0,
-    'fanta_bottle': 0,
-    'cola_cans': 1, 
-    'fanta_cans': 1,
-    'paper_coffee_cups': 2,
-    'water_bottles': 0
-}
+input_shape = (227, 227, 3)
+width = input_shape[0]
+height = input_shape[1]
+channels = input_shape[2]
 
-classes = [ 'coca_cola_bottles', 'fanta_bottle', 'cola_cans', 'fanta_cans', 'paper_coffee_cups', 'water_bottles']
+classes = ['coca_cola_bottles', 'fanta_bottle', 'cola_cans', 'fanta_cans', 'paper_coffee_cups', 'water_bottles']
 
 nb_classes = len(classes)
 
 train_datagen = ImageDataGenerator(
-        rescale=1./255, 
+        rescale=1./255,
+        shear_range=0.2,
+        width_shift_range=0.1,
+        rotation_range=45.,
+        height_shift_range=0.1,
         fill_mode='constant')
 
 test_datagen = ImageDataGenerator(
-        rescale=1./255, 
+        rescale=1./255,
         fill_mode='constant')
 
-train_generator = train_datagen.flow_from_directory('./images', 
-        target_size=(227, 227),
-        batch_size=batch_size,
-        class_mode='categorical', 
-        classes=classes
-        )
+train_generator = train_datagen.flow_from_directory(training_dir,
+                                                    target_size=(width, height),
+                                                    batch_size=batch_size,
+                                                    class_mode='categorical',
+                                                    classes=classes)
+print('train datagen class indices: \n%s' % train_generator.class_indices)
 
-val_generator = test_datagen.flow_from_directory('./images', 
-        target_size=(227, 227),
-        batch_size=batch_size,
-        class_mode='categorical', 
-        classes=classes
-        )
+val_generator = test_datagen.flow_from_directory(val_dir,
+                                                 target_size=(width, height),
+                                                 batch_size=batch_size,
+                                                 class_mode='categorical',
+                                                 classes=classes)
+print('val datagen class indices: \n%s' % val_generator.class_indices)
 
 print('Loading model..')
-model = SqueezeNet(nb_classes, input_shape=(227, 227, 3))
+model = SqueezeNet(nb_classes, width, height, channels)
 # adam = Adam(lr=0.005)
 model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=['accuracy', 'categorical_crossentropy'])
 if os.path.isfile(weights_file):
-    print('Loading weights: %s' % weights_file)
-    model.load_weights(weights_file, by_name=True)
+        print('Loading weights: %s' % weights_file)
+        model.load_weights(weights_file, by_name=True)
 
 print('Fitting model')
-model.fit_generator(train_generator, 
-        samples_per_epoch=samples_per_epoch, 
-        validation_data=val_generator, 
-        nb_val_samples=nb_val_samples, 
-        nb_epoch=nb_epoch, 
-        verbose=1, 
-        initial_epoch=initial_epoch)
+model.fit_generator(train_generator,
+                    samples_per_epoch=samples_per_epoch,
+                    validation_data=val_generator,
+                    nb_val_samples=nb_val_samples,
+                    nb_epoch=nb_epoch,
+                    verbose=1,
+                    initial_epoch=initial_epoch)
 print("Finished fitting model")
 
 print('Saving weights')
 model.save_weights(weights_file, overwrite=True)
-print('Evaluating model')
+# print('Evaluating model')
 #score = model.evaluate_generator(val_generator, val_samples=nb_val_samples)
 #print('result: %s' % score)
 
